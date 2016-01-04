b86939007d81d236ace604bb54de5c14771b9cad
Dave Curylo <curylod@asme.org>
2016-01-04 16:01:03 -0500

    Tidy up code with storm I/O methods

--- StormIntegration.fs
+++ StormIntegration.fs
@@ -1,108 +1,89 @@
 module StormIntegration
 
-open System
-open FSharp.Data
-open FSharp.Data.JsonExtensions
+open System.IO
 open Newtonsoft.Json
 open Newtonsoft.Json.Linq
 
-type SetupInfo = JsonProvider<"Protocol/SetupInfo.json">
-type Pid = JsonProvider<"Protocol/Pid.json">
-type SpoutNextCmd = JsonProvider<"Protocol/SpoutNextCmd.json">
-type SpoutAckCmd = JsonProvider<"Protocol/SpoutAckCmd.json">
-type SpoutFailCmd = JsonProvider<"Protocol/SpoutFailCmd.json">
-type SpoutEmit = JsonProvider<"Protocol/SpoutEmit.json">
-type SpoutLog = JsonProvider<"Protocol/SpoutLog.json">
-type SpoutSyncCmd = JsonProvider<"Protocol/SpoutSyncCmd.json">
-
+type Pid = { pid:int }
 type Emit = { command:string; id:string; stream:int; task:int; tuple:obj array }
-let EmitDoc = { Emit.command="emit"; id="0"; stream=0; task=0; tuple=[||] }
+let EmitDoc = { Emit.command="emit"; id=null; stream=0; task=0; tuple=[||] }
+type Log = { command:string; msg:string }
+let LogDoc = { Log.command="log"; msg=null }
+type Sync = { command:string }
+let SyncDoc = { Sync.command="sync" }
 
 log4net.Config.XmlConfigurator.Configure() |> ignore
-
 let log = log4net.LogManager.GetLogger("StormIntegration")
 
-let readInput (input:IO.TextReader) =
-    let rec getInputMessage (sb:System.Text.StringBuilder) =
+let jsonSerialize o =
+    JsonConvert.SerializeObject (o, Formatting.None, JsonSerializerSettings (DefaultValueHandling=DefaultValueHandling.Ignore))
+
+let jsonDeserialize s = JObject.Parse s
+
+let sendStorm (o:obj) =
+    let os = stdout
+    o |> jsonSerialize |> os.WriteLine
+    "end" |> os.WriteLine
+    os.Flush()
+
+let recvStorm (input:TextReader) =
+    let rec getMessage (sb:System.Text.StringBuilder) =
         match input.ReadLine() with
-        | null
         | "end" -> sb.ToString()
         | s ->
-            sb.AppendLine(s) |> getInputMessage
-    System.Text.StringBuilder() |> getInputMessage
+            sb.AppendLine(s) |> getMessage
+    System.Text.StringBuilder() |> getMessage
 
-let writeEmit () =
-    log.InfoFormat("About to emit()")
-    let os = stdout
-    """{"command":"emit", "tuple":["whatever"]}""" |> os.WriteLine
-    "end" |> os.WriteLine
-    os.Flush()
-    log.InfoFormat("emit complete")
+let writeEmit (tuple:obj array) =
+    { EmitDoc with tuple=tuple } |> sendStorm
 
 let writeLog msg =
-    let os = stdout
-    sprintf """{"command":"log", "msg":"%s"}""" msg |> os.WriteLine
-    "end" |> os.WriteLine
-    os.Flush()
+    { LogDoc with msg = msg } |> sendStorm
 
 let writeSync () =
-    log.InfoFormat("About to sync()")
-    let os = stdout
-    """{"command":"sync"}""" |> os.WriteLine
-    "end" |> os.WriteLine
-    os.Flush()
-    log.InfoFormat("sync complete")
+    SyncDoc |> sendStorm
 
-let rec readmore (input:IO.TextReader) =
-    let line = input |> readInput 
-    log.InfoFormat("Got line: {0}", line)
+let rec readmore (input:TextReader) =
+    let line = input |> recvStorm 
     try 
-        let doc = line |> Newtonsoft.Json.Linq.JObject.Parse
-        writeLog "about to process input."
+        let doc = jsonDeserialize <| line
+        writeLog "Received input, about to process."
         match doc.TryGetValue("command") with
         | true, token -> 
             match token with 
             | :? JValue as jval ->
                 match jval.Value :?> string with
                 | "next" -> 
-                    log.InfoFormat("Got next command, write some tuples!!!!!")
-                    writeEmit ()
-                    writeEmit ()
-                    writeEmit ()
-                    writeEmit ()
-                    writeEmit ()
-                    writeEmit ()
+                    writeLog "Emitting tuples."
+                    let tuples = [|"some";"test";"data";"from";"F#"|] 
+                    tuples |> Array.iter(fun s -> writeEmit [|s|])
+                    sprintf "Emitted %i tuples." tuples.Length |> writeLog
                 | _ -> 
                     failwith "Got unknown command."
             | _ -> 
                 failwith "Got non-string commmand value."
         | false, _ -> 
             failwith (sprintf "Message missing command.: %s" line)
-        System.Threading.Thread.Sleep(1)
+        System.Threading.Thread.Sleep (1)
         writeSync ()
     with
-    | _ as ex -> log.WarnFormat("Line {0} resulted in exception: {1}", line, ex)
+    | _ as ex -> log.WarnFormat ("Input from storm ignored: '{0}'", line, ex)
     input |> readmore
 
 [<EntryPoint>]
 let main argv =
-    log.InfoFormat("Logging initialized.")
-    let line = stdin |> readInput
-    let config = line |> Newtonsoft.Json.Linq.JObject.Parse
+    let line = stdin |> recvStorm
+    let config = jsonDeserialize <| line
     match config.TryGetValue("pidDir") with 
     | true, token ->
         match token with
         | :? JValue as jval ->
             let pidDir = jval.Value :?> string
-            let id = System.Diagnostics.Process.GetCurrentProcess().Id
-            use fs = System.IO.File.Create(System.IO.Path.Combine(pidDir, id.ToString()))
+            let pid = System.Diagnostics.Process.GetCurrentProcess().Id
+            use fs = File.Create(Path.Combine(pidDir, pid.ToString()))
             fs.Close()
-            let os = stdout
-            sprintf """{"pid":%i}""" id |> os.WriteLine
-            "end" |> os.WriteLine
-            os.Flush()
-            log.InfoFormat("Pid sent")
+            { Pid.pid=pid } |> sendStorm
         | _ -> failwith "pidDir had no value."
     | _ -> failwith "Missing pidDir in initial handshake."
     stdin |> readmore
-    0 // return an integer exit code
+    0
