dbc40765d3dcbdd892ac338a471ca46aa15d5817
Dave Curylo <curylod@asme.org>
2016-01-06 10:53:57 -0500

    Bolt deserializes tuple as JSON array.

--- StormBolt.fsx
+++ StormBolt.fsx
@@ -17,12 +17,16 @@ let isHeartbeat (json:JObject) =
 
 let rec processInput (input:TextReader) =
     let line = fromStorm <| input
+    log.InfoFormat("Bolt line: {0}", line)
     try 
         let doc = jsonDeserialize <| line
         logToStorm "Received input, about to process."
         match doc |> isHeartbeat with
-        | true -> syncToStorm ()
+        | true ->
+            "Received heartbeat." |> logToStorm
+            syncToStorm ()
         | false ->
+            "Received tuple" |> logToStorm
             match doc.TryGetValue("id") with
             | true, idToken -> 
                 match idToken with 
@@ -31,19 +35,27 @@ let rec processInput (input:TextReader) =
                     match doc.TryGetValue("tuple") with
                     | true, tupleToken ->
                         match tupleToken with
-                        | :? JValue as jval ->
-                            let tuple = jval.Value :?> obj array
+                        | :? JArray as jarr ->
+                            let tuple = jarr.ToObject<obj[]>()
                             let word = tuple.[0] :?> string
+                            sprintf "Got tuple %s" word |> logToStorm
+                            log.InfoFormat("Got tuple: {0}", word)
                             emitToStorm[|word; word.Length|]
+                            log.InfoFormat("Emitted tuple")
                             ackToStorm tupleId
+                            log.InfoFormat("Ack'd tuple with ID {0}.", tupleId)
+                            "Emitted tuple with length" |> logToStorm
                         | _ -> failwith "Missing tuple data."
                     | _ -> failwith "Missing tuple element."
-                | _ -> 
-                    failwith "Invalid tuple id."
+                | _ ->
+                    "Invalid tuple id" |> logToStorm 
+                    //failwith "Invalid tuple id."
+                    log.ErrorFormat("Invalid tuple id.")
             | false, _ -> 
+                log.ErrorFormat("Missing id.")
                 failwith (sprintf "Message missing id.: %s" line)
     with
-    | _ as ex -> log.WarnFormat ("Input from storm ignored: '{0}'", line, ex)
+    | _ as ex -> log.WarnFormat ("Input from storm ignored: {0} {1}", line, ex)
     input |> processInput
 
 let line = fromStorm <| stdin
